4e5cab8195309331da62f9159e431152cc9104c1
xkrik <krikonis.chr@gmail.com>
2019-05-22 23:07:27 +0300

    Add user component

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import UserBoard from './components/UserBoard'
 
-export default class User extends Component{
-
-  render(){
-    return(
-      <UserBoard name = "Makis" name1 = "Demi" name2 = "Tasos"/>
-    );
+export default class User extends Component {
+  
+  render() {
+    const name = 'Makis'
+    return (<div>
+      <p>User: <b>{ name }</b></p>
+    </div>)
   }
 }
